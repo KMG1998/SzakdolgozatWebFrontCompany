ff1c9a3d8a1c17048a4ce732211a8f5ba39f4528
h766309 <k.mategergo1998@gmail.com>
2024-12-14 12:22:10 +0100

    fixed review table

--- src/components/popup/userDetailsPopUp/pages/UserDetailsReviewsPage.vue
+++ src/components/popup/userDetailsPopUp/pages/UserDetailsReviewsPage.vue
@@ -3,7 +3,7 @@
     class="flex self-stretch min-w-[500px] max-w-[1100px] px-[10px] gap-5 max-md:flex-col max-md:gap-0 max-md:items-stretch text-center">
     <div class="flex flex-col grow items-stretch max-md:mt-10">
       <p class="text-xl font-bold text-black w-full">alkalmazott kapott értékelései</p>
-      <DataTable :table-data="selectedUserStore.userReviews" header-class="reviewTable"/>
+      <DataTable :table-data="selectedUserStore.userReviews" header-class="reviewTable" :field-transformers="fieldTransformers"/>
     </div>
   </div>
 </template>
@@ -11,8 +11,13 @@
 <script setup lang="ts">
 import DataTable from "@/components/commons/DataTable.vue";
 import {useSelectedUserStore} from "@/stores/selectedUser";
+import dateFormatter from "@/utils/dateFormatter";
 
 const selectedUserStore = useSelectedUserStore()
+
+const fieldTransformers = {
+  'createdAt': (dateString) => dateFormatter(dateString)
+}
 
 </script>
 
